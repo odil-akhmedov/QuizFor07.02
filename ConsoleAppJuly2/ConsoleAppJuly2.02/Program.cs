0b54b279f5dd772a0fa916e34c51b30a01684166
Odiljon Akhmedov <akhmedoff.odil@gmail.com>
2014-07-02 16:22:42 -0400

    July 02 Exercises
    Look at the comments

---
+++ ConsoleAppJuly2/ConsoleAppJuly2.02/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppJuly2._02
+{
+    class Book {
+        private int price1;
+
+       public Book(int bookPrice) {
+            this.Price = bookPrice;
+        }
+
+        /*public Book(int price1)
+        {
+            // TODO: Complete member initialization
+            this.price1 = price1;
+        }*/
+        public int Price { get; set; }
+    }
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int price1, price2;
+            Console.WriteLine("Enter the price of the first book: ");
+            price1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the price of the second book: ");
+            price2 = Convert.ToInt32(Console.ReadLine());
+            
+            Book firstBook = new Book(price1);
+            Book secondBook = new Book(price2);
+
+            switch (comparePrices(firstBook, secondBook)) { 
+                case 0:
+                    Console.WriteLine("The prices are the same");
+                    break;
+                case 1:
+                    Console.WriteLine("The first book is more expensive than the second");
+                    break;
+                case -1:
+                    Console.WriteLine("The first book is cheaper than the second");
+                    break;
+                default:
+                    Console.WriteLine("Something is wrong...");
+                    break;
+
+            }
+            Console.ReadLine();
+
+           
+        }
+
+        static int comparePrices(Book book1, Book book2) {
+            if (book1.Price == book2.Price)
+                return 0;
+            if (book1.Price > book2.Price)
+                return 1;
+            if (book1.Price < book2.Price)
+                return -1;
+            return default(int);
+        }
+    }
+}
---
+++ ConsoleAppJuly2/ConsoleAppJuly2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppJuly2
+{
+    class Program
+    {
+        public static int smallest(int a, int b) {
+            if (a < b)
+                return a;
+            else if (b < a)
+                return b;
+            else return default(int);
+        }
+        static void Main(string[] args)
+        {
+            int a, b, result;
+            //Console.Write("Enter two integers followed by 'Enter' each: ");
+            //a = Convert.ToInt32(Console.ReadLine());
+            //b = Convert.ToInt32(Console.ReadLine());
+            //or like this 
+
+            Console.Write("Please two integers separated by a space: ");
+            var allNumbers = Console.ReadLine().Split(' ').Take(2);
+            int[] numbers = allNumbers.Select(d => Convert.ToInt32(d)).ToArray();
+
+            //result = smallest(a, b);
+            //Console.WriteLine("The smallest of 2 integers (" + a + ", " + b + ") you inputted is " + result);
+            result = smallest(numbers[0], numbers[1]);
+            Console.WriteLine("The smallest of 2 integers (" + numbers[0] + ", " + numbers[1] + ") you inputted is " + result);
+
+        }
+    }
+}
